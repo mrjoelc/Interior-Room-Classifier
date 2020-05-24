14379c14775fbfc78c89c584e190dffaa061c6bc
Gioele <gcageggi@gmail.com>
2020-05-24 13:15:47 +0200

    Update Constants.java

--- PyTorchDemoApp/app/src/main/java/org/pytorch/demo/Constants.java
+++ PyTorchDemoApp/app/src/main/java/org/pytorch/demo/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
   public static final String TAG = "PyTorchDemo";
 
   public static String[] CLASSES = new String[]{
-      "cucina","bagno","cameretta","cameraL","salone"
+      "bagno","cucina","cameretta","cameraL","salone"
   };
 }
